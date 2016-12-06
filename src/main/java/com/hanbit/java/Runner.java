f99120ccf13063352681b03130f9b7b67a27afe7
5009 <5009@hb5009>
2016-12-06 15:15:19 +0900

    2차버전

--- src/main/java/com/hanbit/java/Runner.java
+++ src/main/java/com/hanbit/java/Runner.java
@@ -8,10 +8,12 @@ public class Runner {
 		Dog dog = new Dog("진돗개","흰색");
 		Dog dog2 = new Dog("검둥이","도베르만","검정");
 		Dog dog3 = new Dog("바둑이","달마시안","검정/흰색");
+		Dog dog4 = new Dog("바둑이","달마시안","검정/흰색");
 		
 		System.out.println(dog);
 		System.out.println(dog2);
 		System.out.println(dog3);
+		System.out.println(dog4);
 
 		}
 	
